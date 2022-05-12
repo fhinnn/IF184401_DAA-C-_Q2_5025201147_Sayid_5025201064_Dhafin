261e729ce9862d0471633989f4b49c2a10845337
fhinnn_ <deltras12388@gmail.com>
2022-05-12 12:39:19 +0700

    add folder pair3

---
+++ pair3.java
@@ -0,0 +1,15 @@
+public class pair3 {
+
+	public int w,a,b;
+	
+	public pair3(int w,int a, int b){
+		this.a =a;
+		this.b =b;
+		this.w =w;
+	}
+    
+	public String toString(){
+		return "" + this.w;
+	}
+    		
+}
